ae66241f7664593d315f0e77fc1b2fc9ce040b79
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2023-10-26 17:56:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/tt-react-backendless">
     <React.StrictMode>
       <App />
     </React.StrictMode>
